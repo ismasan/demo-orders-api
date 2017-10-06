24eb507fbcf35ba59d018c3621e4b0219a72c4f7
Ismael Celis <ismaelct@gmail.com>
2017-10-06 22:59:15 +0100

    Move repo to its own file

--- api.rb
+++ api.rb
@@ -1,47 +1,12 @@
-require 'securerandom'
 require 'json'
 require 'redis'
 require 'sinatra/base'
 require 'parametric'
 require 'pusher'
-
-module Serializers
-
-end
+require_relative './lib/repo'
 
 Dir["./serializers/*.rb"].each do |f|
   require f
-end
-
-class Paginator
-  attr_reader :items, :offset, :per_page, :total_items
-  def initialize(items: [], offset: 0, per_page: 5, total_items: 0)
-    @items, @offset, @per_page, @total_items = items, offset, per_page, total_items
-  end
-
-  def current_page
-    offset / per_page + 1
-  end
-
-  def next_page?
-    items_so_far < total_items
-  end
-
-  def previous_page?
-    offset > 0
-  end
-
-  def items_so_far
-    current_page * per_page - (per_page - items.size)
-  end
-
-  def next_offset
-    offset + per_page
-  end
-
-  def previous_offset
-    offset - per_page
-  end
 end
 
 ItemSchema = Parametric::Schema.new do
@@ -51,55 +16,6 @@ ItemSchema = Parametric::Schema.new do
 end
 
 REDIS = Redis.new(url: ENV.fetch('REDIS_URL'))
-
-class Repo
-  def initialize(store, &notifier)
-    @store = store
-    @notifier = block_given? ? notifier : ->(chanel, data){}
-  end
-
-  def get(id)
-    data = store.get(id)
-    data ? JSON.parse(data, symbolize_names: true) : nil
-  end
-
-  def set(data)
-    data[:id] = SecureRandom.hex unless data[:id]
-    data[:created_on] = Time.now.iso8601 unless data[:created_on]
-    data[:updated_on] = Time.now.iso8601
-
-    store.set(data[:id], JSON.dump(data))
-    store.zadd 'order_ids', 1, data[:id], nx: true
-
-    notifier.call(:updates, data)
-
-    data
-  end
-
-  def delete(id)
-    store.del id
-    store.zrem 'order_ids', id
-    notifier.call(:deletes, {id: id})
-  end
-
-  def list(offset: 0, per_page: 5)
-    offset = offset.to_i
-    per_page = per_page.to_i
-    total_items = store.zcard('order_ids')
-    range = store.zrange('order_ids', offset, offset + per_page)
-    items = range.map{|id| get(id) }
-
-    Paginator.new(
-      items: items,
-      offset: offset,
-      per_page: per_page,
-      total_items: total_items
-    )
-  end
-
-  private
-  attr_reader :store, :notifier
-end
 
 class Api < Sinatra::Base
   helpers do
